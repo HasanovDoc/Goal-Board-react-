704a65db145e0101dfac345078094dbb76e82ef4
HasanovDoc <hasanov326@mail.ru>
2023-04-09 02:24:48 +0400

    add boardsItem, hide Button

--- src/components/screens/home/WorkPlace/Boards/Boards-item/BoardsItem.jsx
+++ src/components/screens/home/WorkPlace/Boards/Boards-item/BoardsItem.jsx
@@ -1,11 +1,17 @@
-import styles from './BoardsItem.module.css'
+import { useState } from 'react';
+import styles from './BoardsItem.module.css';
+import AnimateHeight from 'react-animate-height';
+import { motion } from "framer-motion"
 
 function BoardsItem(){
     return(
-        <div className={styles.boards_item}>
+        <motion.div className={styles.boards_item} 
+        whileHover={{ height: "auto" }}>
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-            <div>Hello</div>
-        </div>
+                 <div className={styles.item__info}>Hello</div>
+
+            
+        </motion.div>
     )
 }
 
--- src/components/screens/home/WorkPlace/Boards/Boards.jsx
+++ src/components/screens/home/WorkPlace/Boards/Boards.jsx
@@ -1,16 +1,29 @@
 import styles from './Boards.module.css'
 import BoardsItem from './Boards-item/BoardsItem'
+import { useState } from 'react'
+import { motion } from "framer-motion"
+
+const variants = {
+    hide: { x: "-96%" },
+    show: { x: "0" },
+  }
 
 function Boards(){
+    const [isHide, setHide] = useState(false);
+    
     return(
-        <div className={styles.boards}>
-            <BoardsItem />
-            <BoardsItem />
-            <BoardsItem />
-            <BoardsItem />
-            <BoardsItem />
-            <BoardsItem />
-        </div>
+        <motion.div className={styles.boards}
+        animate={isHide ? "hide": "show"}
+        variants={variants}>
+            <div className={styles.item_container}>
+                <BoardsItem />
+                <BoardsItem />
+            </div>
+            <motion.div className={styles.hide_container} onClick={()=>setHide(!isHide)}
+            whileHover={{ scaleX: 1.3 }}>
+
+            </motion.div>
+        </motion.div>
     )
 }
 
